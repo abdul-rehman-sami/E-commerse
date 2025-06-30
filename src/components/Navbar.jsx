73d573ed14b6c396eab8d3ae2a31eb9cc20a09f3
abdul-rehman-sami <abdulrehmansamiofficial@gmail.com>
2025-06-30 22:34:24 +0500

    initial

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <div className='bg-cyan-900 text-white font-semibold'>
             <ul className='flex gap-1 '>
-                <li className='p-2 cursor-pointer hover:bg-cyan-800'>Home</li>
+                <li className='p-2 cursor-pointer hover:bg-cyan-800'>Home!</li>
                 <li className='p-2 cursor-pointer hover:bg-cyan-800'>Products</li>
                 <li className='p-2 cursor-pointer hover:bg-cyan-800'>Catogries</li>
                 <li className='p-2 cursor-pointer hover:bg-cyan-800'>Contact</li>
